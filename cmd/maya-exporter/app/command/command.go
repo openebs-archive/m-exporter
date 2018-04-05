a39acf25ab8b23952c92ad302a110026b45a9e8e
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2018-04-05 14:25:34 +0530

    Rename maya-volume-exporter to maya-exporter
    Changes to be committed:
    	modified:   ../GNUmakefile
    	modified:   ../buildscripts/exporter/Dockerfile
    	modified:   ../buildscripts/exporter/build.sh
    	renamed:    maya-volume-exporter/app/collector/collector.go -> maya-exporter/app/collector/collector.go
    	renamed:    maya-volume-exporter/app/collector/collector_test.go -> maya-exporter/app/collector/collector_test.go
    	renamed:    maya-volume-exporter/app/command/command.go -> maya-exporter/app/command/command.go
    	renamed:    maya-volume-exporter/app/command/exporter.go -> maya-exporter/app/command/exporter.go
    	renamed:    maya-volume-exporter/main.go -> maya-exporter/main.go
    
    1. Why is this change necessary ?
    - Refer to issue openebs/openebs#1379
    
    2. How does this change address the issue ?
    - Rename maya-volume-exporter to maya-exporter wherever used in the code.
    - Rename the same in GNumakefile, buildscripts and Dockerfile.
    
    3. How to verify this change ?
    
    - Run `maya-exporter --help`
    - You should get the output similar to given below
    ```
    maya-exporter can be used to monitor openebs volumes and pools.
    It can be deployed alongside the openebs volume or pool containers as sidecars.
    
    Usage:
       [flags]
    
    Examples:
    maya-exporter -a=http://localhost:8001 -c=:9500 -m=/metrics
    
    Flags:
          --alsologtostderr                  log to standard error as well as files
      -c, --controller.addr string           Address of the Jiva volume controller. (default "http://localhost:9501")
      -h, --help                             help for this command
      -a, --listen.addr string               Address on which to expose metrics and web interface.) (default ":9500")
      -m, --listen.path string               Path under which to expose metrics. (default "/metrics")
          --log-flush-frequency duration     Maximum number of seconds between log flushes (default 5s)
          --log_backtrace_at traceLocation   when logging hits line file:N, emit a stack trace (default :0)
          --log_dir string                   If non-empty, write log files in this directory
          --logtostderr                      log to standard error instead of files (default true)
          --stderrthreshold severity         logs at or above this threshold go to stderr (default 2)
      -v, --v Level                          log level for V logs
          --vmodule moduleSpec               comma-separated list of pattern=N settings for file-filtered logging
    ```
    4. What side effects does this change have ?
    - none
    
    5. Other details :
    - Improvement: openebs/openebs#1379
    
    Signed-off-by: Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>

---
+++ cmd/maya-exporter/app/command/command.go
@@ -0,0 +1,72 @@
+package command
+
+import (
+	goflag "flag"
+
+	"github.com/golang/glog"
+	"github.com/openebs/maya/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+// VolumeExporterOptions is used to create flags for the monitoring command
+type VolumeExporterOptions struct {
+	ListenAddress     string
+	MetricsPath       string
+	ControllerAddress string
+}
+
+// AddListenAddressFlag is used to create flag to pass the listen address of exporter.
+func AddListenAddressFlag(cmd *cobra.Command, value *string) {
+	cmd.Flags().StringVarP(value, "listen.addr", "a", *value,
+		"Address on which to expose metrics and web interface.)")
+}
+
+// AddMetricsPathFlag is used to create flag to pass the listen path where volume
+// metrics are exposed.
+func AddMetricsPathFlag(cmd *cobra.Command, value *string) {
+	cmd.Flags().StringVarP(value, "listen.path", "m", *value,
+		"Path under which to expose metrics.")
+}
+
+// AddControllerAddressFlag is used to create flag to pass the Jiva volume
+// controllers IP.
+func AddControllerAddressFlag(cmd *cobra.Command, value *string) {
+	cmd.Flags().StringVarP(value, "controller.addr", "c", *value,
+		"Address of the Jiva volume controller.")
+}
+
+// NewCmdVolumeExporter is used to create command monitoring and it initialize
+// monitoring flags also.
+func NewCmdVolumeExporter() (*cobra.Command, error) {
+	// create an instance of VolumeExporterOptions to initialize with default
+	// values for the flags.
+	options := VolumeExporterOptions{}
+	options.ControllerAddress = "http://localhost:9501"
+	options.ListenAddress = ":9500"
+	options.MetricsPath = "/metrics"
+	cmd := &cobra.Command{
+		Short: "Collect metrics from OpenEBS volumes",
+		Long: `maya-exporter can be used to monitor openebs volumes and pools.
+It can be deployed alongside the openebs volume or pool containers as sidecars.`,
+		Example: `maya-exporter -a=http://localhost:8001 -c=:9500 -m=/metrics`,
+		Run: func(cmd *cobra.Command, args []string) {
+			util.CheckErr(Run(cmd, &options), util.Fatal)
+		},
+	}
+
+	cmd.Flags().AddGoFlagSet(goflag.CommandLine)
+	goflag.CommandLine.Parse([]string{})
+	AddControllerAddressFlag(cmd, &options.ControllerAddress)
+	AddListenAddressFlag(cmd, &options.ListenAddress)
+	AddMetricsPathFlag(cmd, &options.MetricsPath)
+
+	return cmd, nil
+}
+
+// Run used to process commands,args and call openebs exporter and it returns
+// nil on successful execution.
+func Run(cmd *cobra.Command, options *VolumeExporterOptions) error {
+	glog.Infof("Starting maya-exporter ...")
+	Entrypoint(options)
+	return nil
+}
